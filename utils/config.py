bc13ca1ee5cdb3997b9b74fc15d6359d5cded5a3
Yue Pan <ypan@uni-bonn.de>
2025-01-03 15:36:31 +0100

    minor, sdf pool change

--- utils/config.py
+++ utils/config.py
@@ -113,7 +113,7 @@ class Config:
 
         # local map
         self.diff_ts_local: float = 400.0 # deprecated (use travel distance instead)
-        self.local_map_travel_dist_ratio: float = 5.0
+        self.local_map_travel_dist_ratio: float = 4.0
         self.local_map_radius: float = 50.0
         self.sorrounding_map_radius: float = 100.0
 
@@ -796,7 +796,6 @@ class Config:
         self.infer_bs = self.bs * 8
         self.consistency_count = int(self.bs / 4)
         self.local_map_radius = min(self.max_range*1.1, self.max_range+10.0) # for the local neural points
-        # self.window_radius = max(self.local_map_radius+self.voxel_size_m*2, 6.0) # for the sampling data pool, should not be too small # FIXME
-        self.window_radius = self.local_map_radius * 1.2
+        self.window_radius = max(self.local_map_radius+self.voxel_size_m*2, 6.0) # for the sampling data pool, should not be too small # FIXME
         self.sorrounding_map_radius = self.local_map_radius * 1.8
         self.vis_frame_axis_len = self.max_range / 50.0
